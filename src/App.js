740265f7428318cba679ee52d32d858df0511e79
Carlos Ramirez <carlosjramirez11@gmail.com>
2020-03-28 20:10:39 -0700

    All feaures are now working

--- src/App.js
+++ src/App.js
@@ -26,6 +26,14 @@ class App extends Component {
     ContactsAPI.remove(contact);
   };
 
+  createContact = contact => {
+    ContactsAPI.create(contact).then(contact => {
+      this.setState(prevState => ({
+        contacts: prevState.contacts.concat([contact])
+      }));
+    });
+  };
+
   render() {
     return (
       <div>
@@ -40,7 +48,17 @@ class App extends Component {
           )}
         />
 
-        <Route path='/create' component={CreateContact} />
+        <Route
+          path='/create'
+          render={({ history }) => (
+            <CreateContact
+              onCreateContact={contact => {
+                this.createContact(contact);
+                history.push('/');
+              }}
+            />
+          )}
+        />
       </div>
     );
   }
